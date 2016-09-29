22bf02b52d413eb6e723a0616be9c2066edb28c0
Brandon Xia <brandonxia01@gmail.com>
2016-09-29 16:36:18 -0400

    server stuff
    servertesting3 most legit so far

--- Python Server Stuff/servertest.py
+++ Python Server Stuff/servertest.py
@@ -17,7 +17,9 @@ print 'Serving HTTP on port %s ...' % PORT
 while True:
     client_connection, client_address = listen_socket.accept()
     request = client_connection.recv(1024)
-    print request
+  
+	print request
+
 
     http_response = """\
 HTTP/1.1 200 OK
