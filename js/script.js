1227b5bae8ddb8f652c3f2733e2d8d94af11d807
Tanner Strom <tanner.strom@gmail.com>
2016-10-27 19:42:02 -0400

    what

--- js/script.js
+++ js/script.js
@@ -7,20 +7,3 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
 }
-
-function login() {
-  // Log the user in via Twitter
-  var provider = new firebase.auth.TwitterAuthProvider();
-  firebase.auth().signInWithPopup(provider).catch(function(error) {
-    console.log("Error authenticating user:", error);
-  });
-  // Log the user in via Google
-  
-}
-
-firebase.auth().onAuthStateChanged(function(user) {
-  // Once authenticated, instantiate Firechat with the logged in user
-  if (user) {
-    initChat(user);
-  }
-});
