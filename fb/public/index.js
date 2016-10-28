bd8579094d3744584f058be7e7d706dbe0a9c9eb
egillott <egillott@purdue.edu>
2016-10-27 21:52:30 -0400

    progress on groups

--- fb/public/index.js
+++ fb/public/index.js
@@ -7,46 +7,81 @@ MESSAGE_TEMPLATE =
       '<br><br>' +
     '</div>';
 
+GROUP_TEMPLATE = 
+    '<div class="room-separator"></div>' +
+    '<li class="room-select">' +
+    '<div class="room-title"></div>';
+
 function Whispers() {
   //to do: actual login sequence
-	this.name = "tester";
+  this.name = "tester";
   this.password = "pw";
 
-  //this.groupList = document.getElementById()
+  this.groupList = document.getElementById('groups')
 
   this.messageList = document.getElementById('messages');
-	this.initFirebase();
-	this.loadmessages();
+  this.initFirebase();
+  this.loadmessages();
+  this.loadgroups();
 }
 
 Whispers.prototype.initFirebase = function() {
- 	this.auth = firebase.auth();
-	this.database = firebase.database();
- 	this.storage = firebase.storage();
- 	this.msgref = this.database.ref("messages");
+  this.auth = firebase.auth();
+  this.database = firebase.database();
+  this.storage = firebase.storage();
+  this.msgref = this.database.ref("messages");
+  this.grpref = this.database.ref("groups");
+};
+
+Whispers.prototype.loadgroups = function(e) {
+  var setgroup = function(data) {
+    var x = data.val();
+    console.log(x);
+    Object.keys(x).forEach(function(k) {
+      window.whispers.displaygroup(data.key, x.name);
+    });
+  };
+  this.grpref.limitToLast(12).on('child_added', setgroup);
+}
+
+Whispers.prototype.displaygroup = function(key, name) {
+  var div = document.getElementById(key);
+  //if doesnt exist create it
+  if (!div) {
+     var container = document.createElement('div');
+     container.innerHTML = GROUP_TEMPLATE;
+     div = container.firstChild;
+     div.setAttribute('id', key);
+     window.whispers.groupList.appendChild(div);
+  }
+    div.querySelector('.room-title').textContent = name;
+    //var messageElement = div.querySelector('.message');
+    //messageElement.textContent = text;
+    setTimeout(function() {div.classList.add('visible')}, 1);
+    window.whispers.groupList.scrollTop = window.whispers.groupList.scrollHeight;
 };
 
 Whispers.prototype.loadmessages = function(e) {
-	var setmessage = function(data) {
-		var x = data.val();
-		console.log(x);
-		Object.keys(x).forEach(function(k) {
-			window.whispers.displaymsg(data.key, x.name, x.text);
-		});
-	};
-	this.msgref.limitToLast(12).on('child_added', setmessage);
+  var setmessage = function(data) {
+    var x = data.val();
+    console.log(x);
+    Object.keys(x).forEach(function(k) {
+      window.whispers.displaymsg(data.key, x.name, x.text);
+    });
+  };
+  this.msgref.limitToLast(12).on('child_added', setmessage);
 };
 
 Whispers.prototype.displaymsg = function(key, name, text) {
-	var div = document.getElementById(key);
-	//if doesnt exist create it
-	if (!div) {
- 	   var container = document.createElement('div');
- 	   container.innerHTML = MESSAGE_TEMPLATE;
- 	   div = container.firstChild;
- 	   div.setAttribute('id', key);
-	   window.whispers.messageList.appendChild(div);
-	}
+  var div = document.getElementById(key);
+  //if doesnt exist create it
+  if (!div) {
+     var container = document.createElement('div');
+     container.innerHTML = MESSAGE_TEMPLATE;
+     div = container.firstChild;
+     div.setAttribute('id', key);
+     window.whispers.messageList.appendChild(div);
+  }
     div.querySelector('.name').textContent = name;
     var messageElement = div.querySelector('.message');
     messageElement.textContent = text;
@@ -57,8 +92,8 @@ Whispers.prototype.displaymsg = function(key, name, text) {
 Whispers.prototype.sendmsg = function(name, text) {
     // Add a new message entry to the Firebase Database.
     this.database.ref('messages/').push({
-    	name: name, 
-    	text: text,
+      name: name, 
+      text: text,
     });
 };
 
