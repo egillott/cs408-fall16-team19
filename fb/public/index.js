ddf56063a0835b1cd85eb4a18bb91455abb2fd70
Tanner Strom <tanner.strom@gmail.com>
2016-10-28 21:50:32 -0400

    Revert "try to fix go back button"
    This reverts commit ad9d64d089a11efdfae33ceae078bd17170c1b01.

--- fb/public/index.js
+++ fb/public/index.js
@@ -1,8 +1,9 @@
 //random todo's:
 //add members more easily..?
+//check for duplicates everywhere
+//fix signup grey screen
 //whisper functionality?
 //add alert on (un)successful login / signup (replace log in button with signout / name)
-//fix go back button
 //...done?
 
 MESSAGE_TEMPLATE =
@@ -68,6 +69,8 @@ function login() {
         }
         else {
           alert("Incorrect password");
+
+
         }
       }
     });
@@ -102,6 +105,10 @@ function signup() {
      alert("TWO PASSWORDS DO NOT MATCH!");
      iscorrectsign = 0;
   }
+}
+
+function createUser(name, pass) {
+
 }
 
 function creategroup() {
@@ -276,9 +283,9 @@ $("#new-chat").on("click", function() {
 $("#signup-button").on("click", function() {
   $('#login-menu').hide();
   $('#logout-menu').show();
-  window.location.reload(true);
+  //window.location.reload(true);
     //console.log("Heuy");
-  var modal_signup = new Foundation.Reveal($('#signup-modal'));
+     var modal_signup = new Foundation.Reveal($('#signup-modal'));
   modal_signup.close();
 })
 
@@ -300,9 +307,4 @@ $("#signoutbutton").on("click", function() {
   $('#logout-menu').hide();
   $('#login-menu').show();  
   location.reload(true);
-})
-
-$("#fakesignoutbutton").on("click", function() {
-    var sign_out_modal = new Foundation.Reveal($("#sign-out-modal"));
-    sign_out_modal.close(); 
 })
